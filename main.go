5b6d25f20c94b226b8a9de2e7a71647a0dd964c1
Andrew Pecha <andrew.pecha@gmail.com>
2021-12-26 21:42:57 -0600

    basic get implemented

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Server starting..")
+
+	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/people", getAllPeople)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func getAllPeople(w http.ResponseWriter, r *http.Request) {
+	people := GetPeopleData()
+	json.NewEncoder(w).Encode(people)
+}
+
+func GetPeopleData() []Person {
+	fakeData := []Person{Person{FirstName: "Jacob"}}
+	return fakeData
+}
+
+type Person struct {
+	FirstName string
+}
